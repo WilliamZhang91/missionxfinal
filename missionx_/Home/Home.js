26c50103ccbceab5c5ce195fce38712682da9fcd
AnimateReality <79818477+AnimateReality@users.noreply.github.com>
2021-07-30 01:05:05 +1200

    Add files via upload

---
+++ missionx_/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./Hero";
+import Offer from "./Offer";
+import Skills from "./Skills";
+import Section3 from "./Section3";
+import Waiting from "./Waiting";
+import Footer from "./Footer";
+
+
+function Home() {
+  return (
+    <div>
+    <Hero/>
+    <Offer />
+    <Skills />
+    <Section3 />
+    <Waiting />
+    <Footer/>
+  
+    </div>
+  );
+}
+
+export default Home;
---
+++ missionx_/Home/Waiting.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../App.css";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+function Waiting() {
+  return (
+    <div>
+      <Container style={{ padding: "50px" }} maxWidth="lg">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              {" "}
+              <img
+                src="Mission X Images\Home Page\teacher-4784916_1920.png"
+                alt="class"
+                style={{ width: "90%" }}
+              />
+            </div>
+            <div class="col">
+              <h2
+                style={{
+                  fontWeight: "bold",
+                  textAlign: "left",
+                  paddingTop: "10%",
+                }}
+              >
+                What are you waiting for?
+              </h2>
+              <h3>Start teaching Digital Technologies today.</h3>
+              <p className="section2-p">
+                If you need more information, we are happy to answer any
+                question you may have.{" "}
+              </p>
+              <Button>
+                <img
+                  src="Mission X Images\Home Page\CTA Button.png"
+                  alt="learn more"
+                />
+              </Button>
+              <Button>
+                <img
+                  src="Mission X Images\Home Page\CTA Button-7.png"
+                  alt="learn more"
+                />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Waiting;
