f6e6616cd8783bbf4c88a1e1e767fd0f0de9f083
AnimateReality <79818477+AnimateReality@users.noreply.github.com>
2021-07-30 01:56:09 +1200

    Adding HomePage

--- missionx_/src/App.js
+++ missionx_/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Tab from './component/Tab'
 import ProjectBody from './component/ProjectBody'
+import Home from "./component/Home";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/teacherspage">
             <Tab />
           </Route >
