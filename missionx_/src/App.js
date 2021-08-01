c0c6702b0c02cf8c0b1b73f3bdd207036f890264
WilliamZhang91 <84298284+WilliamZhang91@users.noreply.github.com>
2021-08-02 00:59:52 +1200

    updated app.js version differences

--- missionx_/src/App.js
+++ missionx_/src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Tab from './component/Tab'
-<<<<<<< HEAD
 import ProjectBody from './component/ProjectBody';
-=======
-import ProjectBody from './component/ProjectBody'
->>>>>>> 88803d143d3ec692dd3512252d31027b9b7ccbd7
 import Home from "./component/Home";
 
 
@@ -15,11 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-<<<<<<< HEAD
-        <Route exact path="/" component={Home} />
-=======
           <Route exact path="/" component={Home} />
->>>>>>> 88803d143d3ec692dd3512252d31027b9b7ccbd7
           <Route exact path="/teacherspage">
             <Tab />
           </Route >
